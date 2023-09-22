2aabb90b019e16027eb1779a392b215368f89ba1
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-22 13:34:14 -0400

    Sept 22-Added environment configuration for MySQL (local server)

---
+++ src/main/java/com/example/f23comp1011tasks1/DBUtility.java
@@ -0,0 +1,16 @@
+package com.example.f23comp1011tasks1;
+
+public class DBUtility {
+    private static String user = "student";
+    private static String password = "student";
+
+    /**
+     * jdbc:mysql -> the database driver
+     * 127.0.0.1 -> IP address of the database server
+     * 3306 -> port number of the database server
+     * COMP1011Friday -> name of the database
+     */
+    private static String connectURL = "jdbc:mysql//127.0.0.1:3306/COMP1011Friday";
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.f23comp1011tasks1 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.f23comp1011tasks1 to javafx.fxml;
     exports com.example.f23comp1011tasks1;
