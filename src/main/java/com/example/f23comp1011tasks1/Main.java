ed65e271eb6fefd107eead02809d84679b15099a
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-29 12:31:19 -0400

    Sept 29 - Added some css to the create user view

--- src/main/java/com/example/f23comp1011tasks1/Main.java
+++ src/main/java/com/example/f23comp1011tasks1/Main.java
@@ -3,6 +3,7 @@ package com.example.f23comp1011tasks1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("create-user-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Task Tamer 3000");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/icon.jpg")));
         stage.setScene(scene);
         stage.show();
     }
